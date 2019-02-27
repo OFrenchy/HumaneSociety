bf900d9206951fc1075b70928cae8fc2dc4c62c1
Dean Bourgeois <DeanBourgeois@wi.rr.com>
2019-02-27 12:14:09 -0600

    Changed UserInterface.DisplayAnimalInfo to show room number

--- HumaneSociety/UserInterface.cs
+++ HumaneSociety/UserInterface.cs
@@ -155,7 +155,7 @@ namespace HumaneSociety
         public static void DisplayAnimalInfo(Animal animal)
         {
             Room animalRoom = Query.GetRoom(animal.AnimalId);
-            List<string> info = new List<string>() {"ID: " + animal.AnimalId, animal.Name, animal.Age + " year(s) old", animal.Gender + " gender", "Demeanour: " + animal.Demeanor, "Kid-friendly: " + BoolToYesNo(animal.KidFriendly), "pet-friendly: " + BoolToYesNo(animal.PetFriendly), $"Room: " + animalRoom.RoomId, "Weight: " + animal.Weight.ToString(),  "Food amount in cups: " + (animal.DietPlan == null ? "??" : animal.DietPlan.FoodAmountInCups.ToString()) };
+            List<string> info = new List<string>() {"ID: " + animal.AnimalId, animal.Name, animal.Age + " year(s) old", animal.Gender + " gender", "Demeanour: " + animal.Demeanor, "Kid-friendly: " + BoolToYesNo(animal.KidFriendly), "pet-friendly: " + BoolToYesNo(animal.PetFriendly), $"Room number: " + animalRoom.RoomNumber, "Weight: " + animal.Weight.ToString(),  "Food amount in cups: " + (animal.DietPlan == null ? "??" : animal.DietPlan.FoodAmountInCups.ToString()) };
             DisplayUserOptions(info);
             Console.WriteLine("Press enter to continue.");
             Console.ReadLine();
